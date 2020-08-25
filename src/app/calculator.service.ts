d246ca81f5caff164b3d193e44b8f29499c8e887
mendi perlov <mendi8345@gmail.com>
2020-08-25 17:21:31 +0300

    second commit

--- src/app/calculator.service.ts
+++ src/app/calculator.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class CalculatorService {
 
 
-  result:string;
+  result:string='';
   currentInput:string='';
   previousInput:string='';
   operator:string=''
